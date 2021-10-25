5d7d4b0a48606de9cff18d23d26aebf54c7efd46
sun <shk99061@gmail.com>
2021-10-25 21:48:02 +0900

    save

---
+++ my-app/src/components/atoms/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+type ButtonType={
+    type: any;
+    children: any;
+}
+
+/* export const Button:React.FC<ButtonType> =({type, children})=> {
+    return (
+        <>
+        <button type={type}>{children}</button>
+        </>
+    )
+} */
